4ccb01dc3abfc73750538c29b22ab454ac428d6e
doguebart <douglasweelber.2004@outlook.com>
2023-10-10 12:00:28 -0300

    base project created

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'gold': 'var(--gold)',
+      },
+    },
+  },
+  plugins: [],
+}
